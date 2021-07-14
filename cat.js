4487fb5b43258afc7562ae19b04f7e674520379a
Embox <71951742+emboxity@users.noreply.github.com>
2021-07-14 17:43:41 -0400

    changed to fetch

--- cat.js
+++ cat.js
@@ -1,13 +1,7 @@
-document.querySelector('button').addEventListener('click', catFunction(), {passive: true});
 
-function catFunction(){
-  let xhr = new XMLHttpRequest();
-
-  xhr.open('GET','https://api.thecatapi.com/v1/images/search?size=full');
-
-  xhr.send();
-
-  console.log(xhr);
-  console.log(xhr.response.id);
-
+function catFunction() {
+  const cat = document.getElementById("cat-img");
+  fetch('https://api.thecatapi.com/v1/images/search?size=full')
+  .then(response => response.json())
+  .then(data => cat.src = data[0].url);
 }
